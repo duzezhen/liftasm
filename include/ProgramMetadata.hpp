b9fb02920eed7e58f274e8cd7d67c58bfae53cba
Du <zd233@zzdu.local>
2026-02-19 15:19:01 -0500

    ProgramMetadata

--- include/ProgramMetadata.hpp
+++ include/ProgramMetadata.hpp
@@ -8,7 +8,7 @@ namespace program {
 
 inline constexpr const char* name        = "liftasm";
 inline constexpr const char* description = "Alignment-based coordinate liftover for assemblies and GFA graphs.";
-inline constexpr const char* version     = "0.1";
+inline constexpr const char* version     = "0.1.0";
 inline constexpr const char* build_date  = "2026/02/18";
 inline constexpr const char* author      = "Zezhen Du";
 inline constexpr const char* email       = "dzz0539@gmail.com or dzz0539@163.com";
